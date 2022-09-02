6daf088654ef6e344b1d44e40b9e2b6e2fd13146
ZhangHuiQiang <zhq05410@163.com>
2022-09-02 09:46:49 +0800

    add{day1:基础控件学习}

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+
 }
+
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,105 +15,301 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: '测试',
+      //测试Container
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //      backgroundColor: Colors.brown,
+      //      title: Text('你好',
+      //      textAlign: TextAlign.center,
+      //      style: TextStyle(
+      //        color: Colors.black,
+      //        fontSize: 15.0
+      //      ),),
+      //   ),
+      //   body: Center(
+      //     child: Container(
+      //       width: 300,
+      //       height: 300,
+      //       decoration: BoxDecoration(
+      //         color: Colors.red,
+      //         border: new Border.all(
+      //           color: Colors.green,
+      //           width: 10.0,
+      //         ),
+      //         borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+      //       ),
+      //       child: Text(
+      //         '这是一个容器',
+      //         textAlign: TextAlign.center,
+      //         style: TextStyle(
+      //           color: Colors.yellow,
+      //           fontSize: 20.0
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // ),
+      //测试Image
+      // home: ImageDemo(),
+      //测试Text
+      // home: TextDemo(),
+      //测试Icon
+      // home: IconDemo(),
+      //测试IconButton
+      // home: IconButtonDemo(),
+      //测试ButtonRaisedButton过期
+      // home: RaisedButton1Demo(),
+      //测试ListView
+      // home: ListViewDemo(),
+      //测试水平ListView
+      home: HorizontalListViewDemo(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class ImageDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Center(
+      child: Image.network(
+        'https://img2.baidu.com/it/u=2048195462,703560066&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=333',
+        fit: BoxFit.none,
+        width: 500,
+        height: 500,
+      ),
+    );
+  }
+}
+
+class TextDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('不同字体样式'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(
+            '红色+黑色删除线+25号',
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 25.0,
+              decoration: TextDecoration.lineThrough,
+              decorationColor: Color(0xff000000),
+            ),
+          ),
+          Text(
+            '橙色+下划线+24号',
+            style: TextStyle(
+              color: Color(0xffff9900),
+              decoration: TextDecoration.underline,
+              fontSize: 24.0,
+            ),
+          ),
+          Text(
+            '虚线+上划线+23号+倾斜',
+            style: TextStyle(
+              decoration: TextDecoration.overline,
+              decorationStyle: TextDecorationStyle.dashed,
+              fontSize: 23.0,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            '24号+加粗',
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class IconDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Icon'),
+      ),
+      body: Icon(
+        Icons.alarm,
+        color: Colors.red,
+        size: 50.0,
+      ),
+    );
+  }
+}
+
+class IconButtonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('IconButton'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: IconButton(
+          icon: Icon(
+            Icons.volume_up,
+            size: 30.0,
+            color: Colors.red,
+          ),
+          tooltip: '按下',
+          disabledColor: Colors.blue,
+          onPressed: () { print('===按下'); },
+        ),
+      ),
+    );
+  }
+}
+
+class RaisedButton1Demo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RaisedButton'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            print('===点击');
+          },
+          child: Icon(Icons.phone),
+        ),
+      ),
+    );
+  }
+}
+
+class ListViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ListViewDemo'),
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.alarm, size: 50.0,),
+            title: Text('条目1',
+              style: TextStyle(
+                  color: Colors.green
+              ),),
+            subtitle: Text('条目描述1',
+              style: TextStyle(
+                  color: Colors.red
+              ),),
+            tileColor: Colors.yellow,
+            selectedColor: Colors.blue,
+            selected: true,
+            onLongPress: () {
+              print("===长按");
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.alarm, size: 50.0,),
+            title: Text('条目1',
+              style: TextStyle(
+                  color: Colors.green
+              ),),
+            subtitle: Text('条目描述1',
+              style: TextStyle(
+                  color: Colors.red
+              ),),
+            tileColor: Colors.yellow,
+          ),
+          ListTile(
+            leading: Icon(Icons.alarm, size: 50.0,),
+            title: Text('条目1',
+              style: TextStyle(
+                  color: Colors.green
+              ),),
+            subtitle: Text('条目描述1',
+              style: TextStyle(
+                  color: Colors.red
+              ),),
+            tileColor: Colors.yellow,
+          ),
+          ListTile(
+            leading: Icon(Icons.alarm, size: 50.0,),
+            title: Text('条目1',
+              style: TextStyle(
+                  color: Colors.green
+              ),),
+            subtitle: Text('条目描述1',
+              style: TextStyle(
+                  color: Colors.red
+              ),),
+            tileColor: Colors.yellow,
+          ),
+          ListTile(
+            leading: Icon(Icons.alarm, size: 50.0,),
+            title: Text('条目1',
+              style: TextStyle(
+                  color: Colors.green
+              ),),
+            subtitle: Text('条目描述1',
+              style: TextStyle(
+                  color: Colors.red
+              ),),
+            tileColor: Colors.yellow,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HorizontalListViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 30.0),
+        height: 200.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              width: 100.0,
+              color: Colors.red,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              width: 100.0,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.blue,
+              child: Column(
+                children: <Widget>[
+                  Text('文本',
+                  textAlign: TextAlign.start,),
+                  Icon(Icons.phone)
+                ],
+              ),
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.brown,
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.pinkAccent,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
 }
